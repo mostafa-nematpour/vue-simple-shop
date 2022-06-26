bcc483efcd711f79de5d717d750925e3e53d5c64
Mostafa <nematpor46@gmail.com>
2022-06-26 09:36:37 +0330

    add router and init home page

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: () => import("@/pages/Home.vue"),
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "Error404",
+        component: () => import("@/pages/Error404.vue"),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (to.path === from.path) {
+            return { x: window.scrollX, y: window.scrollY }
+        }
+        document.getElementById('app').scrollIntoView();
+        return { x: 0, y: 0 }
+    },
+});
+
+export default router;
